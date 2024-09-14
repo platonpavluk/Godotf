bc52e06f7bae09d875d50937b873a082f72a40c6
Maks-pro273 <maksderhyn@gmail.com>
2024-09-14 12:41:45 +0300

    Rework Project
    Зробив нормально TileMap, Camera, Parallax and Add New Booster, with double Jump

---
+++ project/Player.gd
@@ -0,0 +1,37 @@
+extends CharacterBody2D
+
+var base_speed = 150.0
+var SPEED = base_speed
+const JUMP_VELOCITY = 200.0
+
+# Jump
+var jump_count = 0
+var max_jumps = 2
+var direction 
+@onready var animation = $Sprite
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	if is_on_floor():
+		jump_count = 0
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and jump_count < max_jumps:
+		velocity.y = -JUMP_VELOCITY
+		jump_count += 1
+
+	var direction := Input.get_axis("Left", "Right")
+	animation.play("run")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		animation.play("idle")
+		
+	if direction < 0:
+		animation.flip_h = true
+	elif direction > 0:
+		animation.flip_h = false
+	move_and_slide()
